80573ca1620dfcee6b8c2affd9fc9a7cc158779b
Daniel Zatovic <daniel.zatovic@gmail.com>
2025-02-19 16:22:21 +0100

    sysext: Allow per-sysext USE flags

--- build_library/extra_sysexts.sh
+++ build_library/extra_sysexts.sh
@@ -3,3 +3,19 @@ EXTRA_SYSEXTS=(
   podman:app-containers/podman,net-misc/passt
   python:dev-lang/python,dev-python/pip
 )
+
+_get_unversioned_sysext_packages_unsorted() {
+  for sysext in "${EXTRA_SYSEXTS[@]}"; do
+    IFS=":" read SYSEXT_NAME PACKAGE_ATOMS USEFLAGS < <(echo "$sysext");
+
+    IFS=, read -ra atoms <<< "$PACKAGE_ATOMS"
+    for atom in "${atoms[@]}"
+    do
+       qatom $atom -F "%{CATEGORY}/%{PN}"
+    done
+  done
+}
+
+get_unversioned_sysext_packages() {
+  _get_package_excludes_unsorted | sort | uniq
+}
